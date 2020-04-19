d89012835c8048f93bd52a60667557dc36f318a9
velladecin <velladecin@hotmail.com>
2020-04-19 23:45:12 +1000

    simplyfied logic, added NewStatNotify

--- stat.go
+++ stat.go
@@ -1,10 +1,10 @@
 package ev
 
 import (
-    "fmt"
     "os"
     "syscall"
     "time"
+    "log"
 )
 
 const (
@@ -15,9 +15,30 @@ type FSObject struct {
     path                    string
     exists, isFile, strict  bool            // file, dir only
     ctime                   int64
-    frequency               int
-    frequencyDuration       time.Duration   // frequency converted to Duration
+    frequency               time.Duration
     callback                func([]interface{})
+}
+
+func NewStatNotify(path string, frequency int, strict bool) *FSObject {
+    return NewStat(path, frequency, strict, func([]interface{}){})
+}
+
+func NewStat(path string, frequency int, strict bool, fn func([]interface{})) *FSObject {
+    if (frequency < 1) {
+        frequency = FREQUENCY
+    }
+
+    exists, isFile, ctime := stat(path)
+
+    return &FSObject {
+        path: path,
+        exists: exists,
+        isFile: isFile,
+        strict: strict,
+        ctime: ctime,
+        frequency: time.Duration(frequency) * time.Second,
+        callback: fn,
+    }
 }
 
 func (fso *FSObject) Notify(notify chan bool) {
@@ -50,63 +71,26 @@ func (fso *FSObject) watch(ch chan bool) {
     for {
         start := time.Now()
 
-        exists, isFile, ctime, err := stat(fso.path)
-        if (err != nil) {
-            // do nothing
-            // TODO log
-            fmt.Printf("stat() on %s failed with: %s", fso.path, err)
-        } else {
-            callback := false
+        exists, isFile, ctime := stat(fso.path)
 
-            if exists != fso.exists {
-                fso.exists = exists
-                callback = true
-            }
+        /* ctime change would cover changes to all the other attributes
+           so check just ctime.. */
 
-            if isFile != fso.isFile {
-                fso.isFile= isFile
-                callback = true
-            }
-
-            if ctime != fso.ctime {
-                fso.ctime = ctime
-                callback = true
-            }
-
-            if callback {
-                ch <- true
-            }
+        if ctime != fso.ctime {
+            fso.ctime = ctime
+            fso.exists = exists
+            fso.isFile = isFile
+            // and nofify..
+            ch <- true
         }
 
         end := time.Now()
         elapsed := end.Sub(start)
-        time.Sleep(fso.frequencyDuration - elapsed)
+        time.Sleep(fso.frequency - elapsed)
     }
 }
 
-func Stat(path string, frequency int, strict bool, fn func([]interface{})) (*FSObject, error) {
-    if (frequency < 1) {
-        frequency = FREQUENCY
-    }
-
-    exists, isFile, ctime, err := stat(path)
-    if err != nil {
-        return nil, err
-    }
-
-    return &FSObject {
-        path: path,
-        exists: exists,
-        isFile: isFile,
-        strict: strict,
-        ctime: ctime,
-        frequency: frequency,
-        frequencyDuration: time.Duration(frequency) * time.Second,
-        callback: fn,
-    }, nil
-}
-
-func stat(path string) (bool, bool, int64, error) {
+func stat(path string) (bool, bool, int64) {
     // not found (default)
     var exists, isFile bool
     ctime := int64(0)
@@ -131,12 +115,12 @@ func stat(path string) (bool, bool, int64, error) {
         }
 
         if t != "" {
-            return isFile, exists, ctime, fmt.Errorf("Accepting file/dir, got(%s)", t)
+            log.Fatalf("Accepting file/dir, got(%s)", t)
         }
 
         s := fi.Sys().(*syscall.Stat_t)
         ctime = int64(s.Ctim.Sec)
     }
 
-    return exists, isFile, ctime, nil
+    return exists, isFile, ctime
 }
